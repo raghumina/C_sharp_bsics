0559fbfc78b32fabbfe5c1ddb2349e52fc000c85
Raghu Mina <raghumina11@gmail.com>
2021-11-11 13:04:45 -0800

    Created first console app in Visual Studio
    First C# project, IDE used is Visual Studio

---
+++ MyFirstConsoleApp/MyFirstConsoleApp/first_console_app.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hey how are you ");
+            Console.WriteLine("every thing ok");
+        }
+    }
+}
